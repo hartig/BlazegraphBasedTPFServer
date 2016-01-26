5a843fc7cd4fb342afd881fb6282444dac6a0132
beebs-systap <beebs@systap.com>
2016-01-26 11:29:54 -0500

    Updates for doc publication.

--- publishDocs.sh
+++ publishDocs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Script to publish javadoc to https://blazegraph.github.com/database/apidocs/
 BASE_DIR=`dirname $0`
-PARENT_POM="${BASE_DIR}/../pom.xml"
+PARENT_POM="${BASE_DIR}/pom.xml"
 DEST_DIR=BlazegraphBasedTPFServer/apidocs/
 #You must have cloned https://github.com/blazegraph/blazegraph.github.io into a directory at the same level as where tinkerpop3 is checked out
-GITHUB_PAGES="${BASE_DIR}/../../blazegraph.github.io"
+GITHUB_PAGES="${BASE_DIR}/../blazegraph.github.io"
 
 if [ ! -d "${GITHUB_PAGES}" ] ; then
 
@@ -16,13 +16,14 @@ fi
 
 mvn -f "${PARENT_POM}" javadoc:javadoc
 
-echo "Javadoc is located in ${BASE_DIR}/../target/site/apidocs/"
+echo "Javadoc is located in ${BASE_DIR}/../target/apidocs/"
 
 pushd `pwd`
 mkdir -p "${GITHUB_PAGES}/${DEST_DIR}"
-echo cp -rf "${BASE_DIR}"/../target/site/apidocs/* "${GITHUB_PAGES}/${DEST_DIR}"
-cp -rf "${BASE_DIR}"/../target/site/apidocs/* "${GITHUB_PAGES}/${DEST_DIR}"
+echo cp -rf "${BASE_DIR}"/target/apidocs/* "${GITHUB_PAGES}/${DEST_DIR}"
+cp -rf "${BASE_DIR}"/target/apidocs/* "${GITHUB_PAGES}/${DEST_DIR}"
 cd $"${GITHUB_PAGES}"
+git pull
 git add --all
 git commit -m "Update for Blazegraph Database Javadocs"
 git push origin master
